780029b2d35660f748e50d96574684af321c67fc
admstt <68070027+admstt@users.noreply.github.com>
2020-07-26 18:36:49 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 rm -rf ~/MonVIP
 mkdir ~/MonVIP
-cd .. && cp -r hgiroll/* ~/MonVIP
-rm -rf hgiroll
+cd .. && cp -r hgiroll-npr/* ~/MonVIP
+rm -rf hgiroll-npr
 cd ~/MonVIP/server
 apt install nodejs -y
 npm install -no-audit; npm install -g pm2 -no-audit && cd .. && rm -rf install.sh
